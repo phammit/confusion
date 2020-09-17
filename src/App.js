84bb079ed0f21d92bbe4526a8401d96cd5939607
minh pham <phammit@gmail.com>
2020-09-17 02:42:28 -0500

    week1 React Components Part 2, lifted state to app.js and added onClick in MenuComponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component} from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes.js';
 
 function App() {
+//course uses class component to deal with state.  can use funtion component and state with hooks
+//useState is a hook that lets you use state in a function component
+// const[] is called "array destructuring"
+const[dishes, setDishes] = useState(DISHES);
+
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -12,7 +18,7 @@ function App() {
           <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={dishes}/>
     </div>
   );
 }
