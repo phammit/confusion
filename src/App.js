3120cc552254042eb7b823fd67d44acefe4e781f
minh pham <phammit@gmail.com>
2020-09-17 00:17:33 -0500

    week 1 react components Part 1 created component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -11,20 +12,7 @@ function App() {
           <NavbarBrand href="/">Ristorant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
     </div>
   );
 }
