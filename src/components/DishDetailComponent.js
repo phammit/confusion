fd6928660284e3f6da436ff2cb5de584ad5c6d8a
minh pham <phammit@gmail.com>
2020-09-17 20:22:15 -0500

    week2 first exercise udpated with date time formatting

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -11,7 +11,7 @@ class DishDetail extends Component {
         return(
             <CardBody key={comment.id}>
                 <CardText>{comment.comment}</CardText>
-                <CardText>{comment.author}, {comment.date}</CardText>
+                <CardText>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</CardText>
             </CardBody>
         );  
     });
